afe198978967c66a4f147ab6148a08c7851d6666
annguyenhuynh <96190092+annguyenhuynh@users.noreply.github.com>
2022-07-25 12:28:41 -0400

    Add files via upload

---
+++ customer_table.sql
@@ -0,0 +1,8 @@
+create table danny_dinner.customer (
+	customer_id varchar,
+	join_date timestamp);
+	
+insert into danny_dinner.customer (customer_id, join_date)
+values 
+	('A', '2021-01-07'),
+	('B', '2021-01-09')
---
+++ menu_table.sql
@@ -0,0 +1,10 @@
+create table danny_dinner.menu (
+	product_id integer,
+	product_name varchar,
+	price integer)
+	
+insert into danny_dinner.menu (product_id, product_name, price)
+values 
+	('1', 'sushi', '10'),
+	('2', 'curry', '15'),
+	('3', 'ramen', '12')
